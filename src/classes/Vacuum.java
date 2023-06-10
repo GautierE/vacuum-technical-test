fef55dbb5a4f935063b6c1b05c4b91a4ee896918
Gautier <gautier.edel66@gmail.com>
2023-06-10 08:12:23 +0200

    Added vacuum move method & related tests

--- src/classes/Vacuum.java
+++ src/classes/Vacuum.java
@@ -3,44 +3,49 @@ package classes;
 import enums.Orientation;
 
 public class Vacuum {
+    private int x;
+    private int y;
     private Orientation orientation;
 
-    public Vacuum(Orientation orientation) {
+    public Vacuum(int x, int y, Orientation orientation) {
+        this.x = x;
+        this.y = y;
         this.orientation = orientation;
     }
 
     public void turnRight() {
         switch (orientation) {
-            case N:
-                orientation = Orientation.E;
-                break;
-            case E:
-                orientation = Orientation.S;
-                break;
-            case S:
-                orientation = Orientation.W;
-                break;
-            case W:
-                orientation = Orientation.N;
-                break;
+            case N -> orientation = Orientation.E;
+            case E -> orientation = Orientation.S;
+            case S -> orientation = Orientation.W;
+            case W -> orientation = Orientation.N;
         }
     }
 
     public void turnLeft() {
         switch (orientation) {
-            case N:
-                orientation = Orientation.W;
-                break;
-            case E:
-                orientation = Orientation.N;
-                break;
-            case S:
-                orientation = Orientation.E;
-                break;
-            case W:
-                orientation = Orientation.S;
-                break;
+            case N -> orientation = Orientation.W;
+            case E -> orientation = Orientation.N;
+            case S -> orientation = Orientation.E;
+            case W -> orientation = Orientation.S;
         }
+    }
+
+   public void move() {
+       switch (orientation) {
+           case N -> y++;
+           case E -> x++;
+           case S -> y--;
+           case W -> x--;
+       }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     public Orientation getOrientation() {
--- src/tests/VacuumTest.java
+++ src/tests/VacuumTest.java
@@ -10,7 +10,7 @@ class VacuumTest {
     @Test
     public void shouldReturnEastIfTurnRightFromNorth()
     {
-        Vacuum vacuum = new Vacuum( Orientation.N);
+        Vacuum vacuum = new Vacuum(0,0, Orientation.N);
         vacuum.turnRight();
         assertEquals(Orientation.E, vacuum.getOrientation());
     }
@@ -18,7 +18,7 @@ class VacuumTest {
     @Test
     public void shouldReturnSouthIf2TurnRightFromNorth()
     {
-        Vacuum vacuum = new Vacuum( Orientation.N);
+        Vacuum vacuum = new Vacuum(0,0, Orientation.N);
         vacuum.turnRight();
         vacuum.turnRight();
         assertEquals(Orientation.S, vacuum.getOrientation());
@@ -27,7 +27,7 @@ class VacuumTest {
     @Test
     public void shouldReturnNorthIf4TurnRightFromNorth()
     {
-        Vacuum vacuum = new Vacuum( Orientation.N);
+        Vacuum vacuum = new Vacuum(0,0, Orientation.N);
         vacuum.turnRight();
         vacuum.turnRight();
         vacuum.turnRight();
@@ -37,14 +37,14 @@ class VacuumTest {
 
     @Test
     public void shouldReturnWestIfTurnLeftFromNorth() {
-        Vacuum vacuum = new Vacuum(Orientation.N);
+        Vacuum vacuum = new Vacuum(0, 0, Orientation.N);
         vacuum.turnLeft();
         assertEquals(Orientation.W, vacuum.getOrientation());
     }
 
     @Test
     public void shouldReturnSouthIf2TurnsLeftFromNorth() {
-        Vacuum vacuum = new Vacuum(Orientation.N);
+        Vacuum vacuum = new Vacuum(0, 0, Orientation.N);
         vacuum.turnLeft();
         vacuum.turnLeft();
         assertEquals(Orientation.S, vacuum.getOrientation());
@@ -52,11 +52,60 @@ class VacuumTest {
 
     @Test
     public void shouldReturnNorthIf4TurnsLeftFromNorth() {
-        Vacuum vacuum = new Vacuum(Orientation.N);
+        Vacuum vacuum = new Vacuum(0, 0, Orientation.N);
         vacuum.turnLeft();
         vacuum.turnLeft();
         vacuum.turnLeft();
         vacuum.turnLeft();
         assertEquals(Orientation.N, vacuum.getOrientation());
+    }
+
+    @Test
+    public void shouldReturn1InYIfMoveFromNorthAndOrigin()
+    {
+        Vacuum vacuum = new Vacuum(0, 0, Orientation.N);
+        vacuum.move();
+        assertEquals(1, vacuum.getY());
+    }
+
+    @Test
+    public void shouldReturn10InYIf10MoveFromNorthAndOrigin()
+    {
+        Vacuum vacuum = new Vacuum(0, 0, Orientation.N);
+        vacuum.move();
+        vacuum.move();
+        vacuum.move();
+        vacuum.move();
+        vacuum.move();
+        vacuum.move();
+        vacuum.move();
+        vacuum.move();
+        vacuum.move();
+        vacuum.move();
+        assertEquals(10, vacuum.getY());
+    }
+
+    @Test
+    public void shouldReturn0InYIfMoveFromSouthAndY1()
+    {
+        Vacuum vacuum = new Vacuum(0, 1, Orientation.S);
+        vacuum.move();
+        assertEquals(0, vacuum.getY());
+    }
+
+    @Test
+    public void shouldReturn1InXIfMoveFromEastAndOrigin()
+    {
+        Vacuum vacuum = new Vacuum(0, 0, Orientation.E);
+        vacuum.move();
+        assertEquals(1, vacuum.getX());
+    }
+
+    @Test
+    public void shouldReturn0InXIfMoveFromWestAndX1()
+    {
+        Vacuum vacuum = new Vacuum(1, 0, Orientation.W);
+        vacuum.move();
+        assertEquals(0, vacuum.getX());
     }
 }
